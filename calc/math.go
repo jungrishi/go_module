bbef8f80fb28e43fbade2ddefc495af73c196c99
Ram Kumar Bhandari <ramjdbhandari51@gmail.com>
2019-12-31 20:42:02 +0545

    Update math.go
    Add return variable on function declaration, so that we can avoid it to return on function body explicitly.

--- calc/math.go
+++ calc/math.go
@@ -1,11 +1,11 @@
 package calc
 
 // Add returns sum of two integer
-func Add(numbers ...int) int {
+func Add(numbers ...int) (sum int) {
 	sum := 0
 
 	for _, num := range numbers {
 		sum += num
 	}
-	return sum
+	return 
 }
